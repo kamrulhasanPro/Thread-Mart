69dace346cd09c44a34567d1c7f3146f0e6487e3
kamrulmiazi <kamrul116522@gmail.com>
2025-12-18 20:07:28 +0600

    get order form details

--- src/Pages/ProductDetails/OrderForm.jsx
+++ src/Pages/ProductDetails/OrderForm.jsx
@@ -9,7 +9,6 @@ const OrderForm = () => {
   const [loading, setLoading] = useState(false);
   const { user } = useAuth();
   const { data: product } = useLoaderData();
-  console.log(product);
   const splitName = user?.displayName.split(" ");
   const firstName = splitName[0];
   const lastName = splitName[1];
@@ -20,9 +19,47 @@ const OrderForm = () => {
     reset,
     control,
   } = useForm();
-
+  console.log(product);
   const handleOrder = (data) => {
-    console.log(data);
+    const {
+      orderQuantity,
+      perPrice,
+      notes,
+      buyerEmail,
+      firstName,
+      lastName,
+      phoneNumber,
+      paymentOption,
+      productName,
+      deliveryAddress,
+    } = data;
+    data.totalPrice = Number(data?.orderQuantity) * Number(data?.perPrice);
+    const newOrder = {
+      managerEmail: product?.managerEmail,
+      productId: product?._id,
+      productName,
+      productPrice: perPrice,
+
+      orderQuantity,
+      totalPrice,
+
+      paymentOption, // COD / Stripe / PayFast
+      paymentStatus: "pending", // pending | paid | failed
+      orderStatus: "pending", // pending | confirmed | shipped | delivered | cancelled
+
+      customer: {
+        firstName,
+        lastName,
+        buyerEmail,
+        phoneNumber,
+        deliveryAddress,
+      },
+
+      notes,
+      createdAt: new Date(),
+    };
+
+    console.log(newOrder);
   };
   const inputBox = (condition) =>
     `w-full border px-4 py-3 rounded-md outline-none border-primary/20 focus:border-primary transition-all validator ${
@@ -34,7 +71,6 @@ const OrderForm = () => {
     defaultValue: product?.moq,
   });
   const totalPrice = watchOrderQuantity * product?.price;
-  console.log(watchOrderQuantity);
   return (
     <section>
       <HeadTitle className={"!mt-0"}>Order Form</HeadTitle>
@@ -43,54 +79,152 @@ const OrderForm = () => {
       <form onSubmit={handleSubmit(handleOrder)}>
         {/* parent */}
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {/* personal information */}
-          <div className="boxStyle">
-            <p>Personal Information</p>
-            <input
-              type="text"
-              value={user?.email}
-              readOnly
-              placeholder="My Email"
-              className={inputBox(errors.buyerEmail)}
-              {...register("buyerEmail", { required: "Enter Your Email" })}
-            />
-            {errors.buyerEmail && (
-              <p className="text-red-400 text-sm">
-                {errors.buyerEmail.message}
-              </p>
-            )}
+          <div className="space-y-4">
+            {/* personal information */}
+            <div className="boxStyle">
+              <p>Personal Information</p>
+              <input
+                type="text"
+                value={user?.email}
+                readOnly
+                placeholder="My Email"
+                className={inputBox(errors.buyerEmail)}
+                {...register("buyerEmail", { required: "Enter Your Email" })}
+              />
+              {errors.buyerEmail && (
+                <p className="text-red-400 text-sm">
+                  {errors.buyerEmail.message}
+                </p>
+              )}
 
-            {/* first and last name */}
-            <div className="flex gap-2 flex-col sm:flex-row">
+              {/* first and last name */}
+              <div className="flex gap-2 flex-col sm:flex-row">
+                <label htmlFor="">
+                  <input
+                    type="text"
+                    placeholder="First Name"
+                    defaultValue={firstName}
+                    className={inputBox(errors.firstName)}
+                    {...register("firstName", {
+                      required: "Enter your FirstName",
+                    })}
+                  />
+                  {errors.firstName && (
+                    <p className="text-red-400 text-sm">
+                      {errors.firstName.message}
+                    </p>
+                  )}
+                </label>
+                <label htmlFor="">
+                  <input
+                    type="text"
+                    placeholder="Your Last Name"
+                    defaultValue={lastName}
+                    className={inputBox(errors.lastName)}
+                    {...register("lastName", {
+                      required: "Your Last Name",
+                    })}
+                  />
+                  {errors.lastName && (
+                    <p className="text-red-400 text-sm">
+                      {errors.lastName.message}
+                    </p>
+                  )}
+                </label>
+              </div>
+            </div>
+
+            {/* product details */}
+            <div className="boxStyle">
+              <p>Product Details</p>
               <label htmlFor="">
+                <p className="text-sm text-gray-300">Product Title</p>
                 <input
                   type="text"
-                  placeholder="First Name"
-                  defaultValue={firstName}
-                  className={inputBox(errors.firstName)}
-                  {...register("firstName", {
-                    required: "Enter your FirstName",
-                  })}
+                  value={product?.productName}
+                  readOnly
+                  placeholder="Product Title"
+                  className={inputBox(errors.productName)}
+                  {...register("productName", { required: "Enter Your Email" })}
                 />
-                {errors.firstName && (
+                {errors.productName && (
                   <p className="text-red-400 text-sm">
-                    {errors.firstName.message}
+                    {errors.productName.message}
                   </p>
                 )}
               </label>
+
+              {/* quantity and price */}
+              <div className="flex gap-2 flex-col sm:flex-row pt-2">
+                <label htmlFor="">
+                  <p className="text-sm text-gray-300">Order Quantity</p>
+                  <input
+                    type="number"
+                    defaultValue={product?.moq}
+                    placeholder="Order Quantity"
+                    className={inputBox(errors.orderQuantity)}
+                    {...register("orderQuantity", {
+                      required: "Enter order quantity",
+                      min: {
+                        value: product?.moq,
+                        message: `Order quantity greater than ${product?.moq}`,
+                      },
+                      max: {
+                        value: product?.availableQuantity,
+                        message: `Order quantity less than ${product?.availableQuantity}`,
+                      },
+                    })}
+                  />
+                  {errors.orderQuantity && (
+                    <p className="text-red-400 text-sm">
+                      {errors.orderQuantity.message}
+                    </p>
+                  )}
+                </label>
+
+                <label htmlFor="">
+                  <p className="text-sm text-gray-300">Par Price</p>
+                  <input
+                    type="number"
+                    placeholder="Per Product Price"
+                    className={inputBox(errors.perPrice)}
+                    value={product?.price}
+                    readOnly
+                    {...register("perPrice", {
+                      required: "Per Price",
+                      min: {
+                        value: 1,
+                        message: "Price must be greater than 0",
+                      },
+                    })}
+                  />
+                  {errors.perPrice && (
+                    <p className="text-red-400 text-sm">
+                      {errors.perPrice.message}
+                    </p>
+                  )}
+                </label>
+              </div>
+
               <label htmlFor="">
+                <p className="text-sm text-gray-300">Total Price</p>
                 <input
                   type="text"
-                  placeholder="Your Last Name"
-                  defaultValue={lastName}
-                  className={inputBox(errors.lastName)}
-                  {...register("lastName", {
-                    required: "Your Last Name",
+                  placeholder="Total Price"
+                  className={inputBox(errors.totalPrice)}
+                  value={`${product?.price} * ${watchOrderQuantity} = ${totalPrice} ৳`}
+                  readOnly
+                  {...register("totalPrice", {
+                    required: "Total Price",
+                    min: {
+                      value: 1,
+                      message: "Price must be greater than 0",
+                    },
                   })}
                 />
-                {errors.lastName && (
+                {errors.totalPrice && (
                   <p className="text-red-400 text-sm">
-                    {errors.lastName.message}
+                    {errors.totalPrice.message}
                   </p>
                 )}
               </label>
@@ -127,18 +261,18 @@ const OrderForm = () => {
                 <input
                   type="text"
                   placeholder="Contact Number"
-                  className={inputBox(errors.paymentOption)}
-                  {...register("contactNumber", {
+                  className={inputBox(errors.phoneNumber)}
+                  {...register("phoneNumber", {
                     required: "Contract number is required",
                     pattern: {
-                      value: /^\+?[1-9]\d{7,14}$/,
-                      message: "Give me valid phone number.",
+                      value: /^\d+$/,
+                      message: "Give me valid phone number without any symbol.",
                     },
                   })}
                 />
-                {errors.contactNumber && (
+                {errors.phoneNumber && (
                   <p className="text-red-400 text-sm">
-                    {errors.contactNumber.message}
+                    {errors.phoneNumber.message}
                   </p>
                 )}
               </label>
@@ -173,102 +307,6 @@ const OrderForm = () => {
               />
               {errors.notes && (
                 <p className="text-red-400 text-sm">{errors.notes.message}</p>
-              )}
-            </label>
-          </div>
-
-          {/* product details */}
-          <div className="boxStyle">
-            <p>Product Details</p>
-            <label htmlFor="">
-              <p className="text-sm text-gray-300">Product Title</p>
-              <input
-                type="text"
-                value={product?.productName}
-                readOnly
-                placeholder="Product Title"
-                className={inputBox(errors.productName)}
-                {...register("productName", { required: "Enter Your Email" })}
-              />
-              {errors.productName && (
-                <p className="text-red-400 text-sm">
-                  {errors.productName.message}
-                </p>
-              )}
-            </label>
-
-            {/* quantity and price */}
-            <div className="flex gap-2 flex-col sm:flex-row pt-2">
-              <label htmlFor="">
-                <p className="text-sm text-gray-300">Order Quantity</p>
-                <input
-                  type="number"
-                  defaultValue={product?.moq}
-                  placeholder="Order Quantity"
-                  className={inputBox(errors.orderQuantity)}
-                  {...register("orderQuantity", {
-                    required: "Enter order quantity",
-                    min: {
-                      value: product?.moq,
-                      message: `Order quantity greater than ${product?.moq}`,
-                    },
-                    max: {
-                      value: product?.availableQuantity,
-                      message: `Order quantity less than ${product?.availableQuantity}`,
-                    },
-                  })}
-                />
-                {errors.orderQuantity && (
-                  <p className="text-red-400 text-sm">
-                    {errors.orderQuantity.message}
-                  </p>
-                )}
-              </label>
-
-              <label htmlFor="">
-                <p className="text-sm text-gray-300">Par Price</p>
-                <input
-                  type="number"
-                  placeholder="Per Product Price"
-                  className={inputBox(errors.perPrice)}
-                  value={product?.price}
-                  readOnly
-                  {...register("perPrice", {
-                    required: "Per Price",
-                    min: {
-                      value: 1,
-                      message: "Price must be greater than 0",
-                    },
-                  })}
-                />
-                {errors.perPrice && (
-                  <p className="text-red-400 text-sm">
-                    {errors.perPrice.message}
-                  </p>
-                )}
-              </label>
-            </div>
-
-            <label htmlFor="">
-              <p className="text-sm text-gray-300">Total Price</p>
-              <input
-                type="text"
-                placeholder="Total Price"
-                className={inputBox(errors.totalPrice)}
-                value={`${product?.price} * ${watchOrderQuantity} = ${totalPrice} ৳`}
-                readOnly
-                {...register("totalPrice", {
-                  required: "Total Price",
-                  min: {
-                    value: 1,
-                    message: "Price must be greater than 0",
-                  },
-                })}
-              />
-              {errors.totalPrice && (
-                <p className="text-red-400 text-sm">
-                  {errors.totalPrice.message}
-                </p>
               )}
             </label>
           </div>
